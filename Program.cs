9d90f3808516053858b9916bce103d8cd0635760
onin sama <ninoabao@gmail.com>
2024-03-21 11:55:43 +0800

    Served the images folder for quick access.

--- Program.cs
+++ Program.cs
@@ -1,55 +1,7 @@
-// using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
-// using pagyeonjaAPI.Entities;
-// var builder = WebApplication.CreateBuilder(args);
-
-// // Add services to the container.
-// builder.Services.AddControllers();
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen(c =>
-// {
-//     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-//     c.OperationFilter<AddUploadFileOperationFilter>(); // Add this line
-// });
-// builder.Services.AddDbContext<HitchContext>();
-// builder.Services.AddCors(options =>
-// {
-//     options.AddDefaultPolicy(builder =>
-//     {
-//         builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-//     });
-// });
-// builder.Services.AddControllers().AddJsonOptions(
-//     options =>
-//     {
-//         options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-//     }
-// );
-
-// var app = builder.Build();
-// app.UseCors();
-
-// // Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI(c =>
-//     {
-//         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); // Add this line
-//     });
-// }
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
-
-// app.MapControllers();
-
-// app.Run();
-
-
 using Microsoft.EntityFrameworkCore;
 using pagyeonjaAPI.Entities;
+using Microsoft.Extensions.FileProviders;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -72,7 +24,19 @@ builder.Services.AddControllers().AddJsonOptions(
     }
 );
 
+// Add static files service
+builder.Services.AddDirectoryBrowser();
+
 var app = builder.Build();
+
+// Use static files middleware
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+    RequestPath = ""
+});
+
 app.UseCors();
 
 // Configure the HTTP request pipeline.
