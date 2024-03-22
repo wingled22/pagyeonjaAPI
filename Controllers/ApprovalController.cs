62c6a83541f0ee14bf912217c0d7bd509e87ca3b
onin sama <ninoabao@gmail.com>
2024-03-22 16:01:42 +0800

    Join the document and rider/commuter to interface the approval requirements.

--- Controllers/ApprovalController.cs
+++ Controllers/ApprovalController.cs
@@ -46,7 +46,7 @@ namespace pagyeonjaAPI.Controllers
                 join r in _context.Riders on a.UserId equals r.RiderId
                 where a.UserType == usertype
                 orderby r.DateApplied
-                select new { a.Id, r.FirstName, r.MiddleName, r.LastName, a.ApprovalStatus, r.ProfilePath }).ToListAsync();
+                select new { a.Id, a.UserId, r.FirstName, r.MiddleName, r.LastName, a.ApprovalStatus, r.ProfilePath }).ToListAsync();
             if (approvals == null)
             {
                 return NotFound();
