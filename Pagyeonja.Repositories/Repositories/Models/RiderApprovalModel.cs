8daddd1332cedb83df70f062b5137f13b9272fee
oninsan <kokoybaldofordawin@gmail.com>
2024-04-02 16:27:53 +0800

    Added some default values for the riderapproval model.

--- Pagyeonja.Repositories/Repositories/Models/RiderApprovalModel.cs
+++ Pagyeonja.Repositories/Repositories/Models/RiderApprovalModel.cs
@@ -6,9 +6,9 @@ public class RiderApprovalModel
 {
     public Guid Id { get; set; }
     public Guid? UserId { get; set; }
-    public string? FirstName { get; set; }
-    public string? MiddleName { get; set; }
-    public string? LastName { get; set; }
-    public bool? ApprovalStatus { get; set; }
-    public string? ProfilePath { get; set; }
+    public string FirstName { get; set; } = "";
+    public string MiddleName { get; set; } = "";
+    public string LastName { get; set; } = "";
+    public bool? ApprovalStatus { get; set; } = null;
+    public string? ProfilePath { get; set; } = null;
 }
