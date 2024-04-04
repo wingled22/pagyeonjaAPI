b68fdbe75afe79d7c30d8109e5e12eb44313d353
Lugubriszxc <@Lugubriszxc>
2024-04-04 10:48:38 +0800

    scaffolded the new 3 tables

---
+++ Pagyeonja.Entities/Entities/RideHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pagyeonja.Entities.Entities;
+
+public partial class RideHistory
+{
+    public Guid RideHistoryId { get; set; }
+
+    public Guid? TransactionId { get; set; }
+
+    public Guid? RiderId { get; set; }
+
+    public Guid? CommuterId { get; set; }
+
+    public Guid? ReviewId { get; set; }
+}
