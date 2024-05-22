4d9cfb07898cc27b22c55ef31f4c215df8d61c0a
oninsan <kokoybaldofordawin@gmail.com>
2024-05-22 11:14:34 +0800

    Done setting up Booking request service.

---
+++ Pagyeonja.Services/Services/BookingRequestService.cs
@@ -0,0 +1,73 @@
+﻿using Pagyeonja.Entities.Entities;
+using Pagyeonja.Repositories;
+using Pagyeonja.Repositories.Repositories;
+
+namespace Pagyeonja.Services;
+
+public class BookingRequestService : IBookingRequestService
+{
+  private readonly IDatabaseTransactionRepository _databaseTransactionRepo;
+  private readonly IBookingRequestRepository _bookingRequestRepository;
+
+  public BookingRequestService(IDatabaseTransactionRepository databaseTransactionRepo, IBookingRequestRepository bookingRequestRepository)
+  {
+    _databaseTransactionRepo = databaseTransactionRepo;
+    _bookingRequestRepository = bookingRequestRepository;
+  }
+  public async Task<BookingRequest> AddBookingRequest(BookingRequest bookingRequest)
+  {
+    using var transaction = await _databaseTransactionRepo.StartTransaction();
+    try
+    {
+      return await _bookingRequestRepository.AddBookingRequest(bookingRequest);
+    }
+    catch (Exception)
+    {
+      await _databaseTransactionRepo.RevertTransaction(transaction);
+      throw;
+    }
+  }
+
+  public async Task BookingRequestResponse(bool response, Guid booking)
+  {
+    using var transaction = await _databaseTransactionRepo.StartTransaction();
+    try
+    {
+      await _bookingRequestRepository.BookingRequestResponse(response, booking);
+    }
+    catch (Exception)
+    {
+      await _databaseTransactionRepo.RevertTransaction(transaction);
+      throw;
+    }
+  }
+
+  public async Task<BookingRequest> GetBookingRequest(Guid booking)
+  {
+    using var transaction = await _databaseTransactionRepo.StartTransaction();
+    try
+    {
+      return await _bookingRequestRepository.GetBookingRequest(booking);
+    }
+    catch (Exception)
+    {
+      await _databaseTransactionRepo.RevertTransaction(transaction);
+      throw;
+    }
+  }
+
+  public async Task<List<BookingRequest>> GetBookingRequests()
+  {
+    using var transaction = await _databaseTransactionRepo.StartTransaction();
+    try
+    {
+      return await _bookingRequestRepository.GetBookingRequests();
+    }
+    catch (Exception)
+    {
+      await _databaseTransactionRepo.RevertTransaction(transaction);
+      throw;
+    }
+  }
+
+}
---
+++ Pagyeonja.Services/Services/IBookingRequestService.cs
@@ -0,0 +1,10 @@
+﻿namespace Pagyeonja.Services;
+using Pagyeonja.Entities.Entities;
+
+public interface IBookingRequestService
+{
+  Task<BookingRequest> AddBookingRequest(BookingRequest bookingRequest);
+  Task<BookingRequest> GetBookingRequest(Guid booking);
+  Task<List<BookingRequest>> GetBookingRequests();
+  Task BookingRequestResponse(bool response, Guid booking);
+}
